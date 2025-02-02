8dc7b90d1179f906c3fd3b0c970e0206b189b1c8
Evandrolds <evandro.lds.ds@outlook.com>
2025-02-02 11:07:42 -0300

    User class added

--- src/main/java/com/content/evandro/course/models/User.java
+++ src/main/java/com/content/evandro/course/models/User.java
@@ -1,4 +1,19 @@
 package com.content.evandro.course.models;
 
+
+import jakarta.persistence.*;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "User_Client")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
 }
