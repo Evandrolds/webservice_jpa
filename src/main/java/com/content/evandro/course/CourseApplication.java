a8056e8bbaf1c3e26da8762ec3136229c531328c
Evandrolds <evandro.lds.ds@outlook.com>
2025-02-04 13:41:29 -0300

    Update Application

--- src/main/java/com/content/evandro/course/CourseApplication.java
+++ src/main/java/com/content/evandro/course/CourseApplication.java
@@ -1,10 +1,16 @@
 package com.content.evandro.course;
 
+import com.content.evandro.course.models.Product;
+import com.content.evandro.course.models.User;
+import com.content.evandro.course.repositories.ProductRepository;
+import com.content.evandro.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CourseApplication {
+public class CourseApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
